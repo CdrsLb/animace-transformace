e84f42de84a9a65d9e4eae7bc9cbac18366cab7c
Luděk Roleček <ludek@rolecek.cz>
2026-05-10 14:38:19 +0200

    Add Vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Odkomentujte cestu ke cvičení, na kterém pracujete:
+
+  root: './01-Transform',
+  // root: './02-Transition',
+  // root: './03-Animation',
+  // root: './04-Foto',
+  // root: './05-Zavody',
+
+
+})
